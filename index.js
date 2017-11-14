80422e2b9cc782831463b21bfe997e480c02aa46
nomanHasan <nomanbinhussein@gmail.com>
2017-11-14 23:37:58 +0600

    Ramda added

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-var terminal = document.querySelector("#console")
-import R from './ramda.js'
-
-
-const logger = (target) => (message) => {
-    target.innerHTML += JSON.stringify(message, null, 4)
+function main() {
+    var terminal = document.querySelector("#console")
+    
+    
+    const logger = (target) => (message) => {
+        target.innerHTML += JSON.stringify(message, null, 4)
+        console.log(message)
+    }
+    
+    const log = logger(terminal)
+    
+    var CARS = [{
+        name: 'Ferrari FF',
+        horsepower: 660,
+        dollar_value: 700000,
+        in_stock: true,
+    }, {
+        name: 'Spyker C12 Zagato',
+        horsepower: 650,
+        dollar_value: 648000,
+        in_stock: false,
+    }, {
+        name: 'Jaguar XKR-S',
+        horsepower: 550,
+        dollar_value: 132000,
+        in_stock: false,
+    }, {
+        name: 'Audi R8',
+        horsepower: 525,
+        dollar_value: 114200,
+        in_stock: false,
+    }, {
+        name: 'Aston Martin One-77',
+        horsepower: 750,
+        dollar_value: 1850000,
+        in_stock: true,
+    }, {
+        name: 'Pagani Huayra',
+        horsepower: 700,
+        dollar_value: 1300000,
+        in_stock: false,
+    }];
+    
+    log(CARS)
+    const valuePerPrice = c => c.horsepower / c.dollar_value
+    const carVPP = R.map(valuePerPrice, CARS)
+    log(carVPP)
+    console.log('awdawd')
 }
-
-const log = logger(terminal)
-
-var CARS = [{
-    name: 'Ferrari FF',
-    horsepower: 660,
-    dollar_value: 700000,
-    in_stock: true,
-}, {
-    name: 'Spyker C12 Zagato',
-    horsepower: 650,
-    dollar_value: 648000,
-    in_stock: false,
-}, {
-    name: 'Jaguar XKR-S',
-    horsepower: 550,
-    dollar_value: 132000,
-    in_stock: false,
-}, {
-    name: 'Audi R8',
-    horsepower: 525,
-    dollar_value: 114200,
-    in_stock: false,
-}, {
-    name: 'Aston Martin One-77',
-    horsepower: 750,
-    dollar_value: 1850000,
-    in_stock: true,
-}, {
-    name: 'Pagani Huayra',
-    horsepower: 700,
-    dollar_value: 1300000,
-    in_stock: false,
-}];
-
-log(R.compose)
