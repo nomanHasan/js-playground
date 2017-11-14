32c509b3fa6a7618abe3c3ad839d5cd40b99d2c6
Noman Hasan <nomanbinhussein@gmail.com>
2017-11-14 18:04:46 +0600

    Ramda Added

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-
 var terminal = document.querySelector("#console")
+import R from './ramda.js'
+
+
+const logger = (target) => (message) => {
+    target.innerHTML += JSON.stringify(message, null, 4)
+}
+
+const log = logger(terminal)
 
 var CARS = [{
     name: 'Ferrari FF',
     horsepower: 660,
     dollar_value: 700000,
     in_stock: true,
-  }, {
+}, {
     name: 'Spyker C12 Zagato',
     horsepower: 650,
     dollar_value: 648000,
     in_stock: false,
-  }, {
+}, {
     name: 'Jaguar XKR-S',
     horsepower: 550,
     dollar_value: 132000,
     in_stock: false,
-  }, {
+}, {
     name: 'Audi R8',
     horsepower: 525,
     dollar_value: 114200,
     in_stock: false,
-  }, {
+}, {
     name: 'Aston Martin One-77',
     horsepower: 750,
     dollar_value: 1850000,
     in_stock: true,
-  }, {
+}, {
     name: 'Pagani Huayra',
     horsepower: 700,
     dollar_value: 1300000,
     in_stock: false,
-  }];
+}];
 
-  
-  terminal.innerHTML = JSON.stringify(CARS,null, 2)
+log(R.compose)
